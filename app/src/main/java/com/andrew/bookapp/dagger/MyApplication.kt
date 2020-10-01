c991e365e9728a56326407667c8fbf3677ebc81d
Andrei Avram <andreia@apadmi.com>
2020-10-01 12:12:07 +0100

    -added amplify framework for authentication

--- app/src/main/java/com/andrew/bookapp/dagger/MyApplication.kt
+++ app/src/main/java/com/andrew/bookapp/dagger/MyApplication.kt
@@ -1,6 +1,12 @@
 package com.andrew.bookapp.dagger
 
 import android.app.Application
+import android.util.Log
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.AuthUserAttributeKey
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.auth.options.AuthSignUpOptions
+import com.amplifyframework.core.Amplify
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.InternalCoroutinesApi
 
@@ -10,6 +16,20 @@ class MyApplication : Application() {
     // Instance of the AppComponent that will be used by all the Activities in the project
     // Factory to create instances of the AppComponent
     // Instance of the AppComponent that will be used by all the Activities in the project
-
-
+    override fun onCreate() {
+        super.onCreate()
+        try {
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+            Amplify.configure(applicationContext)
+            Log.i("MyAmplifyApp", "Initialized Amplify")
+            Amplify.Auth.signUp(
+                    "username",
+                    "Password123",
+                    AuthSignUpOptions.builder().userAttribute(AuthUserAttributeKey.email(), "avramandreitiberiu@gmail.com").build(),
+                    { result -> Log.i("AuthQuickStart", "Result: $result") },
+                    { error -> Log.e("AuthQuickStart", "Sign up failed", error) })
+        } catch (error: AmplifyException) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error)
+        }
+    }
 }
